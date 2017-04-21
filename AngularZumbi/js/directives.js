f304d4c3883d822a9ec894b71e63bd778ab4571a
Marian4 <Mraes.Mariana@gmail.com>
2017-04-20 23:57:07 -0300

    -

--- AngularZumbi/js/directives.js
+++ AngularZumbi/js/directives.js
@@ -34,3 +34,28 @@ app.directive('ngBlur', function() {
         }
     }   
 });
+app.directive('ngInformaCep',function($http){
+    return {
+        restrict: 'A',
+        require: 'ngModel',
+        link: function($scope, $element, $attrs, ngModel) {
+            $scope.$watch($attrs.ngModel, function(value) {
+                if (value) {
+                    if (value.match(/^[0-9]{5}-[0-9]{3}$/)) {
+                        $http.get('http://api.postmon.com.br/v1/cep/' + value).then(function(response) {
+                            console.log(response);
+                            ngModel.$setValidity($attrs.ngModel, true);
+                        });
+                    }
+                    else {
+                        ngModel.$setValidity($attrs.ngModel, false);
+                    }
+                    
+                }
+                else {
+                    ngModel.$setValidity($attrs.ngModel, false);
+                }
+                });
+            }
+        }
+    });
